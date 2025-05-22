98d2ae90a17a2a9be144a914c902c73044b162a5
npcx42 <clownrecords_sup@mail.ru>
2025-05-22 23:38:04 +0300

    fix build errors n4

--- Natives/authenticator/ElyAuthenticator.h
+++ Natives/authenticator/ElyAuthenticator.h
@@ -3,6 +3,9 @@
 // Don't declare interface again as it's already declared in BaseAuthenticator.h
 @interface ElyAuthenticator (Methods)
 
+// Add declaration for initWithData: method
+- (id)initWithData:(NSMutableDictionary *)data;
+
 + (void)downloadLatestAuthlibInjector:(void (^)(BOOL success, NSString *message))callback;
 + (NSString *)getAuthlibInjectorPath;
 
